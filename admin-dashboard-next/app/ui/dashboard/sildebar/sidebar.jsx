df071185f697d6690b39d744ed1157af8af84a2e
hjali7 <hjikingali@gmail.com>
2024-02-15 10:53:45 +0330

    styles for use section in sidebar

--- admin-dashboard-next/app/ui/dashboard/sildebar/sidebar.jsx
+++ admin-dashboard-next/app/ui/dashboard/sildebar/sidebar.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import styles from './sidebar.module.css';
 import menuItems  from './data';
 import MenuLink from './menuLink/menuLink';
@@ -5,11 +7,18 @@ import MenuLink from './menuLink/menuLink';
 export default function Sidebar() {
   return (
     <div className={styles.container}>
+      <div className={styles.user}>
+        <Image src="/assets/noavatar.png" width={50} height={50} alt='UserImage'className={styles.userimg}/>
+        <div className={styles.userDetails}>
+          <span className={styles.username}>John Deo</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
       <ul>
         {
           menuItems.map( category => (
             <li key={category.title}>
-              <span className={styles.cat}>{category.title}</span>
+              <span className={styles.category}>{category.title}</span>
               {
                 category.list.map( item => (
                   <MenuLink item={item} key={item.title} />
