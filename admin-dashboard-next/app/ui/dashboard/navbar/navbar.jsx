8a3caf83dc6ab7514a5b4090880986bc84aa11d2
hjali7 <hjikingali@gmail.com>
2024-02-18 11:42:08 +0330

    navbar structure and style

--- admin-dashboard-next/app/ui/dashboard/navbar/navbar.jsx
+++ admin-dashboard-next/app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+"use client"
+
+import { usePathname } from 'next/navigation';
+
+import { MdNotifications , MdOutlineChat , MdPublic , MdSearch } from 'react-icons/md';
+import styles from './navbar.module.css';
+import Link from 'next/link';
 
 export default function Navbar() {
+  const pathName = usePathname();
   return (
-    <div>navbar</div>
-  )
-}
+      <div className={styles.container}>
+        <div className={styles.title}>{pathName.split("/").pop()}</div>
+        <div className={styles.menu}>
+            <div className={styles.searchbar}>
+            <MdSearch size={15} />
+            <input type="text" placeholder='search...' />
+            </div>
+          <div className={styles.icons} >
+            <Link href="#"><MdOutlineChat size={20} /></Link>
+            <Link href="#"><MdNotifications size={20} /></Link>
+            <Link href="#"><MdPublic size={20} /></Link>
+          </div>
+        </div>
+      </div>
+  );
+};
