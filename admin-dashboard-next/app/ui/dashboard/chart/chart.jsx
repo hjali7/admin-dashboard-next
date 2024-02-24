048fcbd66ee4ae8bdd7a9c30c663bc77a7248052
hjali7 <hjikingali@gmail.com>
2024-02-24 21:32:02 +0330

    add chart and style it

--- admin-dashboard-next/app/ui/dashboard/chart/chart.jsx
+++ admin-dashboard-next/app/ui/dashboard/chart/chart.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+"use client"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import styles from './chart.module.css';
+import { data } from './data';
+
 
 export default function Chart() {
   return (
-    <div>chart</div>
-  )
+      <div className={styles.container}>
+        <h2 className={styles.title}>Weekly Recap</h2>
+          <ResponsiveContainer width="100%" height="90%">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            className={styles.line}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
+            <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
 }
---
+++ admin-dashboard-next/app/ui/dashboard/chart/data.js
@@ -0,0 +1,39 @@
+export const data = [
+  {
+    name: "Sun",
+    click: 4000,
+    visit: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Mon",
+    click: 3000,
+    visit: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Tues",
+    click: 2000,
+    visit: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Wed",
+    click: 2780,
+    visit: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Fri",
+    click: 1890,
+    visit: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Sat",
+    click: 2390,
+    visit: 3800,
+    amt: 2500,
+  },
+];
+
