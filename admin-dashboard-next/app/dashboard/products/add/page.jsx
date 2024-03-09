8d487d713c5db64a383cc78af659d58f958597e9
hjali7 <hjikingali@gmail.com>
2024-03-09 21:13:36 +0330

    add new products page completed

--- admin-dashboard-next/app/dashboard/products/add/page.jsx
+++ admin-dashboard-next/app/dashboard/products/add/page.jsx
@@ -7,28 +7,32 @@ export default function page() {
             <form action=""  className={styles.forms}>
                 <div className={styles.topForm}>
                     <div className={styles.rtForm}>
-                        <input type="text" name="" id="" placeholder='Title'/>
-                        <input type="text" name="" id="" placeholder='Price'/>
-                        <input type="text" name="" id="" placeholder='Color'/>
+                        <input type="text" name="title"  placeholder='Title' required/>
+                        <input type="number" name="price"  placeholder='Price' required/>
+                        <input type="text" name="color"  placeholder='Color' required/>
                     </div>
                     <div className={styles.ltForm}>
-                        <select name="" id="">
-                            <option>Choose a Category</option>
-                            <option>h2</option>
-                            <option>h3</option>
-                            <option>h4</option>
+                        <select name="category" id="category" required>
+                            <option value="general">Choose a Category</option>
+                            <option value="electronic">Electronic</option>
+                            <option value="kitchen">Kitchen</option>
+                            <option value="phone">Phone</option>
+                            <option value="laptop">Laptop</option>
+                            <option value="monitor">Monitor</option>
                         </select>
-                        <input type="number" name="" id=""  placeholder='stock'/>
-                        <div>
-                            <input type="number" name="" id=""  placeholder='size' aria-label='size' aria-describedby='basic-addon2'/>
+                        <input type="number" name="stock" id="stock"  placeholder='Stock'/>
+                        <div className={styles.lenghtForm}>
+                            <input type="number" name="size" id="size"  placeholder='Size' aria-label='size' aria-describedby='basic-addon2'/>
                             <div>
-                                <select name="" id="" disabled="disabled">cm</select>
+                                <select name="" id="">
+                                    <option value="">cm</option>
+                                </select>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <textarea rows="14" cols="100" placeholder='desc'></textarea>
+                <div className={styles.bottomForm}>
+                    <textarea placeholder='Desc' name='desc' id='desc'></textarea>
                 </div>
                 <button type="submit">Submit</button>
             </form>
