78c250ddb3803faeac1c79b39f6464554ff3a6c4
hjali7 <hjikingali@gmail.com>
2024-03-18 12:11:43 +0330

    login page added

--- admin-dashboard-next/app/login/page.jsx
+++ admin-dashboard-next/app/login/page.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import styles from '@/ui/login/login.module.css';
 
 export default function Login() {
   return (
-    <div>page</div>
+    <div className={styles.container}>
+      <form action="">
+        <h2>Login</h2>
+        <input type="text" name="username" id="username" placeholder='username' />
+        <input type="password" name="password" id="password"  placeholder='password'/>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
